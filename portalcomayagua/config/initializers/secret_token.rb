2f9c142ab1657b62e9b852011b9b5f61e150b679
Saúl Eduaro Cabrera <scabrera92@hotmail.com>
2012-05-18 11:43:53 -0600

    created rails project

---
+++ portalcomayagua/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portalcomayagua::Application.config.secret_token = 'ddd316ace4f81f1c49b2a64eee5a43e48e433ea599c682083260569fd9390baac3b8048408c7bd7ed3ed19246c3be7e82bf6b311c30bd1674d4b3e112c84c3c0'
